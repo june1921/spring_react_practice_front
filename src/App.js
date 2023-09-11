32cf7e9d1e2bc2257ed77c7ddf9c8b95eb60dbe5
june1921 <zone1921@naver.com>
2023-09-11 17:00:51 +0900

    프론트엔드를 위한 리액트 프로젝트1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { useContext } from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello React.js!
-        </a>
-      </header>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">
+            Car Shop
+          </Typography>
+        </Toolbar>
+      </AppBar>
     </div>
   );
 }
