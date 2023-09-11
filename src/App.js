14cecfe070cf6f09ef8d04ab2d25404e623f1bf3
june1921 <zone1921@naver.com>
2023-09-11 17:45:47 +0900

    CRUD 기능 첫번째

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-import React from 'react';
-import { useContext } from 'react';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import { useContext } from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Carlist from "./components/Carlist";
 
 function App() {
-  return (
-    <div className="App">
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6">
-            Car Shop
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    </div>
-  );
+    return (
+        <div className="App">
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography variant="h6">Car Shop</Typography>
+                </Toolbar>
+            </AppBar>
+            <Carlist />
+        </div>
+    );
 }
 
 export default App;
